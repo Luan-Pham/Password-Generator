4e75fb76634de27a30eb7010ea4857f56c7c6d3d
Luan Pham <luanpham@Luans-MacBook-Pro.local>
2022-07-04 13:55:57 -0700

    first version of working code

--- script.js
+++ script.js
@@ -8,21 +8,24 @@ function generatePassword (){
   var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" .split('')
   var genNumber = "123456789" .split('')
   var symbols = "!@#$%^&*()_+=/?.><" .split('')
-  var PWlength = prompt("Please enter length between 8-128", "8-128")
+  var PWlength = prompt("Please enter length of password between 8-128", "8-128")
   var asklowerCase = prompt ("Pass with lowercase letters?", "yes/no")
   var askUppercase = prompt("Password with uppercase letters?", "yes/no")
   var askNumbers = prompt("Password with numbers?","yes/no")
   var askSymbols = prompt ("Password with symbols?", "yes/no")
   var PW = []
 
+  // create string of possible characters to select from depending on select submission in prompts
   var Pwstring= []
   if (asklowerCase == "yes") Pwstring = Pwstring.concat(lowerCase)
   if (askUppercase == "yes") Pwstring = Pwstring.concat(upperCase)  
   if (askNumbers == "yes") Pwstring = Pwstring.concat(genNumber)
   if (askSymbols == "yes") Pwstring = Pwstring.concat(symbols)
-  console.log(PWstring)
-  for (let i=0; i <= PWlength - 1 ; i++){
-    var passwordcharacter = Pwstring[Math.floor(Math.random() * PWlength)
+  console.log(Pwstring)
+
+  // for loop in order to select each character from string of possible characters
+  for (let i=1; i <= PWlength ; i++){
+    var passwordcharacter = Pwstring[Math.floor(Math.random() * Pwstring.length)
     ]
     PW.push(passwordcharacter) 
     var finalpass = PW.join('')
@@ -34,10 +37,5 @@ function generatePassword (){
   
   }
 
-// function writePassword() {
-//   var password = generatePassword() ;
-//   var passwordText = document.querySelector("#password");
-//   passwordText.value = password;
-// }
-// Add event listener to generate button
+  // click event to generate password
 generateBtn.addEventListener("click", generatePassword);
